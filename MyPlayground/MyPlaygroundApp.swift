0a6b3bbd058ed4124d389792990141037253505f
woojin <woojin1900@gmail.com>
2023-01-31 22:21:43 +0900

    Initial Commit

---
+++ MyPlayground/MyPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPlaygroundApp.swift
+//  MyPlayground
+//
+//  Created by denver on 2023/01/31.
+//
+
+import SwiftUI
+
+@main
+struct MyPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
